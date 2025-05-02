202a51ea67f7db73293e8d413c78c6178f1a84e7
Ystellaeikonozumatrolezi12a <ystellaeii@gmail.com>
2025-05-02 13:14:45 -0300

    resolução do exercício combustível

--- pages/consumo/consumo.js
+++ pages/consumo/consumo.js
@@ -3,7 +3,6 @@ function calculoDoConsumo(distancia, combustivel) {
     return `${consumoMedio.toFixed(3)} km/l`
 }
 
-
 let resultado1 = calculoDoConsumo(500, 35.0)
 let resultado2 = calculoDoConsumo(2254, 124.4)
 let resultado3 = calculoDoConsumo(4554, 464.6)
